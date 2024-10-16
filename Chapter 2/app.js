8d50b9628bdc1a0de07a79a1a9da07ad618287ab
Ayan <160330735+Ayan-devv@users.noreply.github.com>
2024-10-16 22:45:39 +0500

    Add files via upload

---
+++ Chapter 2/app.js
@@ -0,0 +1,22 @@
+var username;
+
+var myName = "Muhammad Ayan";
+
+var message;
+message = "Hello World";
+
+alert(message);
+
+var email;
+email = "dev.muhammadayan@gmail.com";
+alert(email);
+
+var book;
+book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book" + email);
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+var design;
+design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
