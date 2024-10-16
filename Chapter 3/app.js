0c965851553e5648667c5cb466e23cd1443fcef7
Ayan <160330735+Ayan-devv@users.noreply.github.com>
2024-10-16 23:02:22 +0500

    Add files via upload

---
+++ Chapter 3/app.js
@@ -0,0 +1,25 @@
+var age;
+age = 17;
+alert("I am" + " " + age + " " + "years old");
+
+var numberOfVisits;
+numberOfVisits = 14;
+alert("You have visited this site " + numberOfVisits + " times")
+
+
+var birthYear;
+birthYear = 2006;
+document.write("My birth year is" + birthYear)
+
+document.write("Data type of my declared variable is" + " " + typeof(birthYear));
+
+var visitorsName;
+var productTitle;
+var quantity;
+
+visitorsName = "John";
+productTitle = "T-Shirts"
+quantity = 5;
+
+alert(`${visitorsName} ordered ${quantity} ${productTitle} On XYZ Clothing Store`);
+
