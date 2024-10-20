d174875bf65217da5f6ff66b39ca4d413d0b06a6
Ayan <160330735+Ayan-devv@users.noreply.github.com>
2024-10-20 23:36:42 +0500

    Add files via upload

---
+++ Chapter 9 to 11/app.js
@@ -0,0 +1,28 @@
+var city = prompt("Enter your City Name");
+
+if (city.toLowerCase == karachi ) {
+    alert("Welcome to city of lights")
+} else {
+    alert("Welcome to " + city);
+};
+
+var gender = prompt("Enter your Gender Male/Female");
+
+if (gender.toLowerCase == male) {
+    alert("Good morning Sir")
+} else if (gender.toLowerCase == female) {
+    alert("Good morning Ma'am")
+} else {
+    alert("Good morning");
+};
+
+
+var fuel = prompt("Enter the amount of remaining fuel in your car:");
+
+if (fuel <= 0.25) {
+    alert("Please Refuel your car");
+} else {
+    alert("You have enough fuel");
+}
+
+
